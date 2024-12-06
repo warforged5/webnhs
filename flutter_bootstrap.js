812fc53e6f0f6aa9a8706afbaff1acae7e6e2396
Chase <148923937+warforged5@users.noreply.github.com>
2024-12-06 15:27:16 -0500

    again again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2650697201"
+    serviceWorkerVersion: "166309450"
   }
 });
